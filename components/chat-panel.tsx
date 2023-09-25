1e046e0979d38d804ad65e9842a4749fcfbd8f0e
Priyansh Goel <priyanshgoel05@gmail.com>
2023-09-25 11:27:55 +0530

    added prompts

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -60,6 +60,26 @@ export function ChatPanel({
         <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
           <PromptForm
             onSubmit={async value => {
+              if (value.toLowerCase().includes("generate") || value.toLowerCase().includes("draft")){
+                value = "Task: Draft a professional legal document like contracts, wills, legal notices, court documents, etc. specified in the user input, delimited by triple backticks.\n" +
+                    "\n" +
+                    "Input:\n" +
+                    "The type of legal document (e.g., contract, will, legal notice, court document, etc.)\n" +
+                    "A description of the legal document, including the following information:\n" +
+                    "The names and addresses of the parties involved\n" +
+                    "The key terms and conditions of the document\n" +
+                    "Any other relevant details\n" +
+                    "\n" +
+                    "Output:\n" +
+                    "A professional legal document that adheres to the guidelines specified by Indian law and is legally viable in the Indian court of law.\n" +
+                    "\n" +
+                    "Additional guidelines for legal documents:\n" +
+                    "The language used in the document must be clear and concise, and easy to understand for all parties involved.\n" +
+                    "The document must be comprehensive and cover all of the relevant terms and conditions.\n" +
+                    "The document must be drafted in compliance with all applicable Indian laws and regulations.\n" +
+                    "The document must adhere to the guidelines specified by Indian law.\n" + value
+              }
+
               await append({
                 id,
                 content: value,
