a895c04b722d1bf7944f3d145eb90738e64df81c
Lala Arnav Vatsal <lalaarnav003@gmail.com>
2023-10-01 16:40:19 +0530

    again

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -11,7 +11,32 @@ const exampleMessages = [
   },
   {
     heading: 'Draft a Legal Document',
-    message: 'Draft a Legal document for me with the following specifications: \n'
+    message: 'Task: Draft a professional legal document like contracts, wills, legal notices, court documents, etc. specified in the user input, delimited by triple backticks.\n' +
+      '\n' +
+      'Input:\n' +
+      'The type of legal document (e.g., contract, will, legal notice, court document, etc.)\n' +
+      'A description of the legal document, including the following information:\n' +
+      'The names and addresses of the parties involved\n' +
+      'The key terms and conditions of the document\n' +
+      'Any other relevant details\n' +
+      '\n' +
+      'Output:\n' +
+      'A professional legal document that adheres to the guidelines specified by Indian law and is legally viable in the Indian court of law.\n' +
+      '\n' +
+      'Additional guidelines for legal documents:\n' +
+      'The language used in the document must be clear, concise, and easy to understand for all parties involved.\n' +
+      'The document must be comprehensive and cover all of the relevant terms and conditions.\n' +
+      'The document must be drafted in compliance with all applicable Indian laws and regulations.\n' +
+      'The document must adhere to the guidelines specified by Indian law.\n' +
+      '\n' +
+      'For the above instructions, determine if anything needs to be clarified. \n' +
+      '\n' +
+      'If yes, do not carry them out. You can make reasonable assumptions, but if you are unsure, ask questions in short for clarification. If there are multiple questions, ask them one by one. If you still need more information, keep asking again until you have all the required information. DO NOT OUTPUT PLACEHOLDER TEXT IN THE OUTPUT DOCUMENT.\n' +
+      '\n' +
+      'If nothing else needs to be clarified, output the document. \n'+
+      '```\n' +
+      '\n' +
+      '```\n'
   },
 ]
 
